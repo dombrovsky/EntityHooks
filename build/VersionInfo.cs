96567215228109d0440b8065aa489b6ec4532b0c
Volodymyr Dombrovskyi <vladimir@goortom.com>
2014-02-02 20:21:55 +0200

    Version updated to 1.0.0-beta

--- build/VersionInfo.cs
+++ build/VersionInfo.cs
@@ -12,4 +12,4 @@
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0-alpha")]
+[assembly: AssemblyInformationalVersion("1.0.0-beta")]
