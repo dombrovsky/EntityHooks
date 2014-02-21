b73501490d632ae876eb3952d937c1d0b0c6cda7
Volodymyr Dombrovskyi <vladimir@goortom.com>
2014-02-21 20:44:20 +0200

    Fix #3. Redundant line removed from build.bat

--- build.bat
+++ build.bat
@@ -5,8 +5,6 @@ set msbuild=C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe
 set projectfile=build\build.proj
 
  :start
-call %BuildCommonDir%\prebuild.bat
-
 set target=
 if NOT "%1" == "" set target=/t:%1
  %msbuild% %_loglevel% %target% %projectfile%
