d631260c7686c3f00f9ee646e72d35f82b570a72
Volodymyr Dombrovskyi <vladimir@goortom.com>
2014-03-23 02:14:27 +0200

    Version changed to 1.1.0-beta

--- build/VersionInfo.cs
+++ build/VersionInfo.cs
@@ -11,8 +11,8 @@ using System.Reflection;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyInformationalVersion("1.1.0-beta")]
 
 [assembly: SuppressMessage("Microsoft.Usage", "CA2243:AttributeStringLiteralsShouldParseCorrectly", Justification = "Need to include suffix in AssemblyInformationalVersion.")]
